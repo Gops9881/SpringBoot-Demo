ba5674217fa452f4e98f71b34b7a51c94f825da1
gopalcool <kulkarnigopal20@gmail.com>
2019-11-30 11:45:55 +0100

    added swagger to projects

--- src/main/java/com/umati/springboot/Controller/UserController.java
+++ src/main/java/com/umati/springboot/Controller/UserController.java
@@ -3,6 +3,9 @@ package com.umati.springboot.Controller;
 import com.umati.springboot.customexceptions.ResourceNotFoundException;
 import com.umati.springboot.model.User;
 import com.umati.springboot.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,36 +16,37 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//@Api(value = "User Managment System",description = "It is used to manage the user data")
+@Api(value = "User Managment System",description = "It is used to manage the user data")
 public class UserController {
     @Autowired
     UserService userService;
-    //@ApiOperation(value = "List all user from the System", response = List.class)
+    @ApiOperation(value = "List all user from the System", response = List.class)
     @RequestMapping(value = "/users" ,method = RequestMethod.GET)
     public ResponseEntity<List<User>> listAllUsers() {
         List<User> list= userService.findAllUser();
         return new ResponseEntity<List<User>>(list,HttpStatus.OK);
     }
-    //@ApiOperation(value = "Get the user details from the system with specifid user id",response = User.class) @ApiParam(value = "User id of the user to fetch",required = true)
+    @ApiOperation(value = "Get the user details from the system with specifid user id",response = User.class)
     @RequestMapping(value = "/user/{id}" ,method = RequestMethod.GET)
-    public ResponseEntity<User> getUser( @PathVariable("id") long id) throws ResourceNotFoundException {
+    public ResponseEntity<User> getUser(@ApiParam(value = "User id of the user to fetch",required = true) @PathVariable("id") long id) throws ResourceNotFoundException {
+
         User list= userService.findById(id);
         return new ResponseEntity<User>(list,HttpStatus.OK);
     }
-    //@ApiOperation(value="Create the user to the System",response = User.class) @ApiParam(value = "User Information to be added",required = true)
+    @ApiOperation(value="Create the user to the System",response = User.class)
     @RequestMapping(value = "/user",method = RequestMethod.POST)
-    public User createUser( @Valid @RequestBody User user){
+    public User createUser( @ApiParam(value = "User Information to be added",required = true) @Valid @RequestBody User user){
         return userService.addUser(user);
     }
-    //@ApiOperation(value = "User information to be Updated",response = User.class) @ApiParam(value = "User id to be updated",required = true)
+    @ApiOperation(value = "User information to be Updated",response = User.class)
     @RequestMapping(value = "/user/{id}",method = RequestMethod.PUT )
-    public ResponseEntity<User> updateUser( @PathVariable(value = "id") long id,@Valid @RequestBody User user) throws ResourceNotFoundException{
+    public ResponseEntity<User> updateUser(@ApiParam(value = "User id to be updated",required = true) @PathVariable(value = "id") long id,@Valid @RequestBody User user) throws ResourceNotFoundException{
         User userUpdated=userService.updateUser(id,user);
         return ResponseEntity.ok(userUpdated);
     }
-    //@ApiOperation(value = "User delete from the System") @ApiParam(value = "User id to be deleted from the system",required = true )
+    @ApiOperation(value = "User delete from the System")
     @RequestMapping(value="/user/{id}",method = RequestMethod.DELETE)
-    public Map<String, Boolean> deleteUser( @PathVariable(value = "id") long id) throws ResourceNotFoundException{
+    public Map<String, Boolean> deleteUser(@ApiParam(value = "User id to be deleted from the system",required = true ) @PathVariable(value = "id") long id) throws ResourceNotFoundException{
             return userService.deleteUser(id);
     }
 }
